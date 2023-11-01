c4becbc0ed3ba862366cac9e1bfad0bb56826842
Daniel Gehlen <harmonia251251@gmail.com>
2023-11-01 20:09:56 -0300

    My project

--- src/components/layout/navbar/NavBar.js
+++ src/components/layout/navbar/NavBar.js
@@ -4,40 +4,46 @@ import styles from './NavBar.module.css';
 
 
 function Navbar() {
+
+  const linkStyle = {
+    color: 'white',
+    textDecoration: 'none', // Remova a sublinhado padrão
+  };
+
   return (
 <div className={styles.NavBar}>
   <div className={styles.NavItem}>
-    <Link to="/">Home</Link>
+    <Link to="/" style={linkStyle}>Home</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section2">Clock</Link>
+    <Link to="/section2" style={linkStyle}>Clock</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section3">Separate Buttons</Link>
+    <Link to="/section3" style={linkStyle}>SeparateButtons</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section4">Gallery</Link>
+    <Link to="/section4" style={linkStyle}>Gallery</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section5">Mirror Button</Link>
+    <Link to="/section5" style={linkStyle}>MirrorButton</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section6">Profiles</Link>
+    <Link to="/section6" style={linkStyle}>Profiles</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section7">ToDoList Profiles</Link>
+    <Link to="/section7" style={linkStyle}>ToDoLists</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section8">About Page</Link>
+    <Link to="/section8" style={linkStyle}>About</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section9">Shoping List</Link>
+    <Link to="/section9" style={linkStyle}>ShopingList</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section10">Tic tac Toe</Link>
+    <Link to="/section10" style={linkStyle}>TicTac</Link>
   </div>
   <div className={styles.NavItem}>
-    <Link to="/section11">Filterable Product</Link>
+    <Link to="/section11" style={linkStyle}>FilterableProducts</Link>
   </div>
 </div>
 
