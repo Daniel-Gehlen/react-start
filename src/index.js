e433acae8b36d59839be34baaa257b1bd8e87492
Daniel Gehlen <harmonia251251@gmail.com>
2023-11-03 20:26:09 -0300

    My project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { CounterContextProvider } from "./context/CounterContext";
+import { TitleColorContextProvider } from "./context/TitleColorContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/* 2 - criando provider */}
+    <CounterContextProvider>
+      <TitleColorContextProvider>
+        <App />
+      </TitleColorContextProvider>
+    </CounterContextProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Greeting from '../components/greetings/Greeting';
+import { useContext } from 'react';
+import {CounterContext} from '../context/CounterContext';
+import ChangeCounterContext from '../components/buttons/ChangeCounterContext';
 
 
-function Home() {
+const Home = () => {
+  const { counter } = useContext(CounterContext);
+
+
   return (
     <div>
           <Greeting name="world" />
           <h1>Welcome to my app</h1>
+          <p>Permanent counter value: {counter}</p>
+          <ChangeCounterContext />
     </div>
   );
-}
+};
 
 export default Home;
